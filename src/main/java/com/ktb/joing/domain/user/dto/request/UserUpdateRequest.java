f088c59899b949eec7c81d9f8865b8db5e860360
hyoom <ghkddbstj@naver.com>
2024-12-04 15:33:44 +0900

    JOING-47 refactor: @Email 어노테이션의 불필요한 정규식 제거

--- src/main/java/com/ktb/joing/domain/user/dto/request/UserUpdateRequest.java
+++ src/main/java/com/ktb/joing/domain/user/dto/request/UserUpdateRequest.java
@@ -17,7 +17,7 @@ public class UserUpdateRequest {
     @Size(max = 20, message = "닉네임은 최대 20자까지 가능합니다")
     private String nickname;
 
-    @Email(regexp = "^[A-Za-z0-9+_.-]+@(.+)$", message = "올바른 이메일 형식이 아닙니다")
+    @Email(message = "올바른 이메일 형식이 아닙니다")
     private String email;
 
     private MediaType mediaType;

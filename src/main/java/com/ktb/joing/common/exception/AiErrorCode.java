3ea6da748f8d61f299461ce4d3d22df1964e8ef3
hyoom <ghkddbstj@naver.com>
2024-12-14 16:46:10 +0900

    JOING-57 refactor: AI 에러 통신 처리 방식 개선
    - 에러 메시지 기반에서 에러 코드 기반으로 개선

--- src/main/java/com/ktb/joing/common/exception/AiErrorCode.java
+++ src/main/java/com/ktb/joing/common/exception/AiErrorCode.java
@@ -11,9 +11,9 @@ public enum AiErrorCode implements ErrorCode {
     AI_VALIDATION_ERROR(HttpStatus.UNPROCESSABLE_ENTITY, "입력값 검증에 실패했습니다"),
     AI_BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다"),
     AI_BAD_GATEWAY(HttpStatus.BAD_GATEWAY, "AI 서버와의 통신에 실패하였습니다."),
-    AI_INVALID_CHANNEL_ID_FORMAT(HttpStatus.UNPROCESSABLE_ENTITY, "유효하지 않은 형식의 채널아이디입니다"),
-    AI_INVALID_CHANNEL_ID(HttpStatus.UNPROCESSABLE_ENTITY, "유효하지 않은 채널아이디입니다"),
-    AI_INSUFFICIENT_VIDEOS(HttpStatus.UNPROCESSABLE_ENTITY, "영상의 개수가 충분하지 않아 더이상의 평가가 불가능합니다");
+    AI_INVALID_CHANNEL_ID_FORMAT(HttpStatus.UNPROCESSABLE_ENTITY, "유효하지 않은 형식의 채널아이디입니다."),
+    AI_INVALID_CHANNEL_ID(HttpStatus.UNPROCESSABLE_ENTITY, "유효하지 않은 형식의 채널아이디입니다."),
+    AI_NOT_ENOUGH_UPLOADS(HttpStatus.UNPROCESSABLE_ENTITY, "영상의 개수가 충분하지 않아 더이상의 평가가 불가능합니다.");
 
 
     private final HttpStatus httpStatus;
--- src/main/java/com/ktb/joing/common/util/webClient/AIErrorDetail.java
+++ src/main/java/com/ktb/joing/common/util/webClient/AIErrorDetail.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class AIErrorResponse {
-    private AIErrorDetail detail;
+public class AIErrorDetail {
+    private String code;
+    private String message;
 }

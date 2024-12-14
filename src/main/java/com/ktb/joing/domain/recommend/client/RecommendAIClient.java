c423ea2de91e635a43fc82ea584d3985b0194bd7
hyoom <ghkddbstj@naver.com>
2024-12-14 21:24:25 +0900

    JOING-58 feat: AI 추천 엔드포인트 변경

--- src/main/java/com/ktb/joing/domain/recommend/client/RecommendAIClient.java
+++ src/main/java/com/ktb/joing/domain/recommend/client/RecommendAIClient.java
@@ -23,7 +23,7 @@ public class RecommendAIClient {
     // 기획안으로 -> 크리에이터 추천 받음
     public Mono<CreatorRecommendResponse> requestCreatorRecommend(CreatorRecommendRequest request) {
         return reactiveHttpService.post(
-                aiUrl + "/rec/recommend/item",
+                aiUrl + "/rec/recommend/creator",
                 request,
                 CreatorRecommendResponse.class
         ).doOnError(e -> log.error("AI 크리에이터 추천 요청 실패: {}", e.getMessage()));
@@ -32,7 +32,7 @@ public class RecommendAIClient {
     // 크리에이터로 -> 기획안 추천 받음
     public Mono<ItemRecommendResponse> requestItemRecommend(ItemRecommendRequest request){
         return reactiveHttpService.post(
-            aiUrl + "/rec/recommend/creator",
+            aiUrl + "/rec/recommend/item",
                 request,
                 ItemRecommendResponse.class
         ).doOnError(e -> log.error("AI 기획안 추천 요청 실패: {}", e.getMessage()));

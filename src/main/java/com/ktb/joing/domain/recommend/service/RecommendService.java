76cf1da94bf708e7f9d10e5430e4f4363cd124d2
hyoom <ghkddbstj@naver.com>
2024-12-15 14:30:31 +0900

    JOING-58 feat: AI 필드 변경사항 반영 및 임시로 작성했던 로직 삭제

--- src/main/java/com/ktb/joing/domain/recommend/service/RecommendService.java
+++ src/main/java/com/ktb/joing/domain/recommend/service/RecommendService.java
@@ -12,7 +12,6 @@ import com.ktb.joing.domain.recommend.dto.response.ItemRecommendResponse;
 import com.ktb.joing.domain.recommend.exception.RecommendErrorCode;
 import com.ktb.joing.domain.recommend.exception.RecommendException;
 import com.ktb.joing.domain.user.entity.Creator;
-import com.ktb.joing.domain.user.entity.MediaType;
 import com.ktb.joing.domain.user.exception.UserErrorCode;
 import com.ktb.joing.domain.user.exception.UserException;
 import com.ktb.joing.domain.user.repository.CreatorRepository;
@@ -67,13 +66,10 @@ public class RecommendService {
     }
 
     private CreatorRecommendRequest creatorRecommendRequest(Item item) {
-        String mediaType = null;
-        if (item.getMediaType()==MediaType.LONG_FORM) mediaType = "long";
-        else if (item.getMediaType()==MediaType.SHORT_FORM) mediaType = "short";
         return CreatorRecommendRequest.builder()
                 .title(item.getTitle())
                 .category(item.getCategory().toString().toLowerCase())
-                .mediaType(mediaType) //.mediaType(item.getMediaType().toString().toLowerCase())
+                .mediaType(item.getMediaType().toString().toLowerCase())
                 .score(item.getScore())
                 .content(item.getContent())
                 .build();

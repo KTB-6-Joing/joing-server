cc0d3991f270499641323b7d6d39bb95cedc6585
hyoom <ghkddbstj@naver.com>
2024-12-19 13:42:14 +0900

    JOING-65 fix: 기획안 추천 서비스 데이터 조회 로직 개선
    - LazyInitializationException 문제 해결
    - DTO 기반 쿼리 메소드 추가
    - 트랜잭션 설정 최적화

--- src/main/java/com/ktb/joing/domain/item/repository/ItemRepository.java
+++ src/main/java/com/ktb/joing/domain/item/repository/ItemRepository.java
@@ -1,12 +1,22 @@
 package com.ktb.joing.domain.item.repository;
 
 import com.ktb.joing.domain.item.entity.Item;
+import com.ktb.joing.domain.recommend.dto.response.ItemRecommendView;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
+    @Query("SELECT new com.ktb.joing.domain.recommend.dto.response.ItemRecommendView(" +
+            "i.title, " +
+            "i.content, " +
+            "i.summary.keyword) " +
+            "FROM Item i WHERE i.id = :itemId")
+    Optional<ItemRecommendView> findItemRecommendViewById(@Param("itemId") Long itemId);
 
     List<Item> findByProductManagerUsernameAndCreatedDateTimeGreaterThanEqualOrderByCreatedDateTimeDesc(
             String username, LocalDateTime startDate

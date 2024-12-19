70526871ccf0f68f19ecb8ef02821c1518c01ae0
hyoom <ghkddbstj@naver.com>
2024-12-19 13:49:00 +0900

    JOING-65 refactor: 크리에이터 추천 서비스의 DTO 조회 최적화
    - 크리에이터 조회 시 필요한 필드만 DTO로 직접 조회하도록 개선

--- src/main/java/com/ktb/joing/domain/user/repository/CreatorRepository.java
+++ src/main/java/com/ktb/joing/domain/user/repository/CreatorRepository.java
@@ -1,10 +1,20 @@
 package com.ktb.joing.domain.user.repository;
 
+import com.ktb.joing.domain.recommend.dto.response.CreatorRecommendView;
 import com.ktb.joing.domain.user.entity.Creator;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface CreatorRepository extends JpaRepository<Creator, Long> {
     Optional<Creator> findByUsername(String username);
+
+    @Query("SELECT new com.ktb.joing.domain.recommend.dto.response.CreatorRecommendView(" +
+            "c.profileImage, " +
+            "c.nickname, " +
+            "c.channelUrl) " +
+            "FROM Creator c WHERE c.id = :creatorId")
+    Optional<CreatorRecommendView> findCreatorRecommendViewById(@Param("creatorId") Long creatorId);
 }

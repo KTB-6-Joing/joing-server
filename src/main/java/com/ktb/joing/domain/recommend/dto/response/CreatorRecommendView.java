e427da02019cc88202930b76d2f71737819eb893
hyoom <ghkddbstj@naver.com>
2025-01-07 21:51:58 +0900

    JOING-67 feat: 매칭 중복 요청 방지 로직 추가

--- src/main/java/com/ktb/joing/domain/recommend/dto/response/CreatorRecommendView.java
+++ src/main/java/com/ktb/joing/domain/recommend/dto/response/CreatorRecommendView.java
@@ -12,13 +12,23 @@ public class CreatorRecommendView {
     private String profileImage;
     private String nickname;
     private String channelUrl;
+    private boolean isMatched;
 
-    @Builder
+    // JPQL용 생성자 추가
     public CreatorRecommendView(Long id, String profileImage, String nickname, String channelUrl) {
         this.id = id;
         this.profileImage = profileImage;
         this.nickname = nickname;
         this.channelUrl = channelUrl;
+    }
+
+    @Builder
+    public CreatorRecommendView(Long id, String profileImage, String nickname, String channelUrl, boolean isMatched) {
+        this.id = id;
+        this.profileImage = profileImage;
+        this.nickname = nickname;
+        this.channelUrl = channelUrl;
+        this.isMatched = isMatched;
     }
 }
 
